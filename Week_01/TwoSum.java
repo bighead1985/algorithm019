0dbc14e62a500de2b4b2efcd03ba133caec2580c
liqing <nickbest85_hk@163.com>
2020-11-08 20:23:29 +0800

    两数之和

--- Week_01/TwoSum.java
+++ Week_01/TwoSum.java
@@ -1,10 +1,35 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-/**
- * Description:
- * User: liqing@pluosi
- * Date: 2020-11-08
- * Time: 7:52 PM
- */
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+//
+//
+//
+// 示例:
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+//
+// Related Topics 数组 哈希表
 public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int len = nums.length;
+        Map<Integer, Integer> hashMap = new HashMap<>(len - 1);
+        hashMap.put(nums[0],0);
+        for (int i = 1; i < len; i++) {
+            int another = target - nums[i];
+            if (hashMap.containsKey(another)) {
+                return new int[]{i, hashMap.get(another)};
+            }
+            hashMap.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solustion");
+
+    }
 }
