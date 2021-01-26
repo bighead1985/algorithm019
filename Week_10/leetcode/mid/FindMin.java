9db8945202fdfef416b356801cc3d0e14333c898
liqing <nickbest85@126.com>
2021-01-26 10:40:46 +0800

    searchII

--- Week_10/leetcode/mid/FindMin.java
+++ Week_10/leetcode/mid/FindMin.java
@@ -1,10 +1,11 @@
 package leetcode.mid;
 
 /**
- * Description:
+ * Description:153. 寻找旋转数组中的最小值
  * User: liqing@pluosi
  * Date: 2021-01-25
  * Time: 7:20 PM
  */
 public class FindMin {
+
 }
--- Week_10/leetcode/mid/SearchII.java
+++ Week_10/leetcode/mid/SearchII.java
@@ -7,4 +7,45 @@ package leetcode.mid;
  * Time: 7:18 PM
  */
 public class SearchII {
+    public boolean search(int[] nums, int target) {
+
+        if (null == nums || nums.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target == nums[mid]) {
+                return true;
+            }
+
+            if (nums[start] == nums[mid]) {
+                start++;
+                continue;
+            }
+
+
+            if (nums[start] < nums[mid]) {
+
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+
+            }
+        }
+        return false;
+
+    }
 }
